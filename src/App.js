bdf09555560c86c3919e35e2ac760f47b64c79bc
jenny obryant <jennyobryant@protonmail.com>
2019-09-15 21:17:50 -0600

    3

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Route } from 'react-router-dom'; 
 
-//import './App.css'; 
+
 
 import TabNav from "./components/TabNav.js";
 import Header from "./components/Header.js";
--- src/components/LocationCard.js
+++ src/components/LocationCard.js
@@ -2,10 +2,10 @@ import React from "react";
 
 export default function LocationCard(props) {
   return <span>
+
     <div> { props.data.name }</div>
     <div> { props.data.type }</div>
     <div> { props.data.dimension }</div>
-    <div> { props.data.residents }</div> 
-
+ 
   </span>;
 }
